ea037a3124a6795ed1d74134cd5b9318e24d3bff
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-08-11 20:07:18 -0700

    add config for netlify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from "axios";
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
@@ -8,9 +9,15 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas)
 
+if (process.env.REACT_APP_API_BASE_URL) {
+    axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <App />
     </BrowserRouter>
 );
+
+
+
