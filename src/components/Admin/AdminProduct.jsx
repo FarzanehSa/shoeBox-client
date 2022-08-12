57781a242d5b72ee84a0fe4e886bc526e9dd433c
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-08-11 20:36:01 -0700

    clean code

--- src/components/Admin/AdminProduct.jsx
+++ src/components/Admin/AdminProduct.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useContext} from 'react';
+import React, {useState, useContext} from 'react';
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 
