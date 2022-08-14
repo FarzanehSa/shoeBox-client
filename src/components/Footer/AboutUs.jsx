4648a408d0d73d4807453a1266b5a8f376e8bc1c
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-08-14 16:46:43 -0700

    import khaled changes

--- src/components/Footer/AboutUs.jsx
+++ src/components/Footer/AboutUs.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function AboutUs() {
+  
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <div className="main-layout">
       <div className='data'>
--- src/index.js
+++ src/index.js
@@ -14,3 +14,24 @@ root.render(
         <App />
     </BrowserRouter>
 );
+
+/* if (process.env.REACT_APP_API_BASE_URL) {
+    setUrl("https://shoe-box-api.herokuapp.com");
+  } else {
+    setUrl(`http://localhost:${process.env.REACT_APP_SERVER_PORT || 8100}`);
+  }
+
+// -----
+
+useEffect(() => {
+    if (matchDashboard) {
+      setTitle("Shoe Box Dashboard");
+    } else {
+      setTitle("Shoe Box");
+    }
+  },[matchDashboard]);
+
+
+  const [url, setUrl] = useState("https://shoe-box-api.herokuapp.com");
+  const [title, setTitle] = useState("Shoe Box");
+  console.log('⭐️ v.02'); */
