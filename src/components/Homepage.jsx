e1930ce117789840d048625b20c2f6fd5c6f496e
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-08-17 11:52:16 -0700

    changed homepage photos

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,16 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Homepage.scss";
 
 const Homepage = () => {
 
+  const [photo1, setPhoto1] = useState(1)
+  const [photo2, setPhoto2] = useState(10)
+  const [photo3, setPhoto3] = useState(100)
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
+  console.log(photo1, '  ',photo2, '  ',photo3)
+  useEffect(() => {
+    const rotatePhoto = setTimeout(() => {
+      const sum = photo1 + photo2 + photo3;
+      switch (sum) {
+        case 111:
+          setPhoto1(2);
+          break;
+        case 112:
+          setPhoto2(20);
+          break;
+        case 122:
+          setPhoto3(200);
+          break;
+        case 222:
+          setPhoto1(1);
+          break;
+        case 221:
+          setPhoto2(10);
+          break;
+        default:
+          setPhoto3(100);
+          break;
+      }
+    },10000);
+    
+    return (() => clearTimeout(rotatePhoto))
+  },[photo1, photo2, photo3])
 
   return (
     <div style={{lineHeight:'1.6'}} className="homepage-layout">
       <div className="image-container">
-        <img className="main-image" src="../main-image-3.png" alt="mainImage" />
+        <img className="main-image" src={`../homeBackground-${photo1}.jpg`} alt="mainImage" />
+        <img className="main-image" src={`../homeBackground-${photo2}.jpg`} alt="mainImage" />
+        <img className="main-image" src={`../homeBackground-${photo3}.jpg`} alt="mainImage" />
       </div>
       <div className="text-under-image">
         <p className="data-title">
